cae24cd8f427d3e9e506ece4f09c25883d7194f3
Murat Kurtboğan <murat.kurtbogan@odeontechnology.com>
2020-06-04 02:14:53 +0300

    Token Expired Test

---
+++ SennedjemTests/Services/Authentication/TokenTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using SennedjemTests.Helpers.TokenHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Claims;
+using System.Net.Http.Headers;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace SennedjemTests.Services.Authentication
+{
+    [TestFixture]
+    public class TokenTest : BaseIntegrationTest
+    {
+        [Test]
+        public async System.Threading.Tasks.Task TokenAthorizeTest()
+        {
+            var token = MockJwtTokens.GenerateJwtToken(GetClaims());
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _client.GetAsync("api/animals/getall");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task TokenExpiredTest()
+        {
+            var token = MockJwtTokens.GenerateJwtToken(GetClaims());
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            Thread.Sleep(10000);
+
+            var response = await _client.GetAsync("api/animals/getall");
+            Assert.AreEqual(HttpStatusCode.Unauthorized, response.StatusCode);
+
+        }
+
+        public List<Claim> GetClaims()
+        {
+            return new List<Claim>()
+            {
+                new Claim("username", "deneme"),
+                new Claim("email", "test@test.com")
+
+            };
+        }
+
+    }
+}
