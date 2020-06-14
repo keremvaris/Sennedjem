38b88725b8ca416a94b80315c718b9b878fad09b
Emre <emre@edakik.com>
2020-06-14 10:00:59 +0300

    Business dependency injection işlemleri için AddBusiness metodu oluşturuldu.

---
+++ Business/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+
+namespace Business
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusiness(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
