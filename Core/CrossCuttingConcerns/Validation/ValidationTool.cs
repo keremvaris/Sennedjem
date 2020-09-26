12fdf5b7137216c3fc0a97538ad72dc161d62b9d
Murat Kurtboğan <murat.kurtbogan@odeontechnology.com>
2020-09-27 01:55:36 +0300

    Validation edit

--- Core/CrossCuttingConcerns/Validation/ValidationTool.cs
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -6,7 +6,9 @@ namespace Core.CrossCuttingConcerns.Validation
     {
         public static void Validate(IValidator validator, object entity)
         {
-            var result = validator.Validate(entity);
+            var context = new ValidationContext<object>(entity);
+
+            var result = validator.Validate(context);
             if (!result.IsValid)
             {
                 throw new ValidationException(result.Errors);
