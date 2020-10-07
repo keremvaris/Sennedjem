ad19aac25b69bc67eaeaa9dfb8a01b4533559789
Murat Kurtboğan <murat.kurtbogan@odeontechnology.com>
2020-10-07 18:50:46 +0300

    EnumExtensions

---
+++ SennedjemTests/Core/Extensions/EnumExtensionTests.cs
@@ -0,0 +1,32 @@
+﻿using Core.Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SennedjemTests.Core.Extensions
+{
+    [TestFixture]
+    public class EnumExtensionTests
+    {
+        [Test]
+        public void GetDescriptionTest()
+        {
+            var description = UserType.Admin.GetDescription();
+
+            Assert.That(description, Is.EqualTo("Admin"));
+        }
+    }
+
+    public enum UserType
+    {
+        [System.ComponentModel.Description("Admin")]
+        Admin=1,
+
+        [System.ComponentModel.Description("Guess")]
+        Guess =2,
+
+        [System.ComponentModel.Description("Office")]
+        Office =3
+    }
+}
