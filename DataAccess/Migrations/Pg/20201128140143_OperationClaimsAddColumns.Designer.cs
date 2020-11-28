eda5a4088a09cb4a07df38cb45e56d581d2a43a8
Murat Kurtboğan <murat.kurtbogan@odeontechnology.com>
2020-11-28 17:27:17 +0300

    Add OperationClaimsAliasAndDescriptionColumns

--- DataAccess/Migrations/Pg/20201128140143_OperationClaimsAddColumns.Designer.cs
+++ DataAccess/Migrations/Pg/20201128140143_OperationClaimsAddColumns.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using DataAccess.Concrete.EntityFramework.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace DataAccess.Migrations.Pg
 {
     [DbContext(typeof(ProjectDbContext))]
-    partial class ProjectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201128140143_OperationClaimsAddColumns")]
+    partial class OperationClaimsAddColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
