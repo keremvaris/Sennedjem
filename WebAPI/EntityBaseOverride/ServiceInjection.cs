be94ca280ce6ac77e7e5d350ab91b2dfd2214cb5
Murat Kurtboğan <murat.kurtbogan@odeontechnology.com>
2020-12-02 15:20:05 +0300

    Add EntityBase override

---
+++ WebAPI/EntityBaseOverride/ServiceInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.EntityBaseOverride
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ServiceInjection : IDesignTimeServices
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        public void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ICSharpEntityTypeGenerator, OverrideBase>();
+        }
+    }
+}
+
